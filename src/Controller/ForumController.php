f9e5d496fd089bbdf64c94147d25a1c6335592dc
Lord-forgos <johan.rousseau39@gmail.com>
2023-05-25 14:54:56 +0200

    add controller

---
+++ src/Controller/ForumController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ForumCategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ForumController extends AbstractController
+{
+    private ForumCategoryRepository $categoryRepository;
+    private SerializerInterface $serializer;
+
+    public function __construct(ForumCategoryRepository $categoryRepository, SerializerInterface $serializer)
+    {
+        $this->categoryRepository = $categoryRepository;
+        $this->serializer = $serializer;
+    }
+
+    #[Route('/api/forums/categories', name: 'get_forum_getAllForumCategories', methods: 'GET')]
+    public function getAllForumCategories(): Response
+    {
+        $categoriesJson = $this->serializer->serialize($this->categoryRepository->findAll(), "json",
+            ["groups" => "forumcategory_read"]);
+        return new JsonResponse($categoriesJson, Response::HTTP_OK, [], true);
+    }
+}
