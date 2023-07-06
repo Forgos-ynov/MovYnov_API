744f12ac691067bd3dde0463ac2aed1d84feaa44
Lord-forgos <johan.rousseau39@gmail.com>
2023-07-06 16:52:27 +0200

    add new TMDB path

--- src/Controller/TmdbController.php
+++ src/Controller/TmdbController.php
@@ -9,11 +9,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class TmdbController extends AbstractController
 {
     private string $prefixeUrl;
+    private string $prefixeUrlSearch;
     private string $sufixeUrl;
 
     public function __construct()
     {
         $this->prefixeUrl = "https://api.themoviedb.org/3/movie/";
+        $this->prefixeUrlSearch = "https://api.themoviedb.org/3/search/";
         $this->sufixeUrl = "?append_to_response=credits,watch/providers,videos&language=fr-FR&api_key=b8e285eb83d6d2fccd9f0acc529b5004";
     }
 
@@ -37,6 +39,15 @@ class TmdbController extends AbstractController
     public function getInformationsOfOneMovie(int $idMovie): JsonResponse
     {
         $url = $this->prefixeUrl . $idMovie . $this->sufixeUrl;
+        $json = $this->getJsonFromUrl($url);
+        return $this->json($json);
+    }
+
+    #[Route('/api/tmdb/movies/searchName/{searchName}', name: 'get_tmdb_getMoviesByName', methods: "GET")]
+    public function getMoviesByName(string $searchName): JsonResponse
+    {
+        $url = $this->prefixeUrlSearch . "movie" . $this->sufixeUrl . "&query=" . $searchName;
+        var_dump($url); die();
         $json = $this->getJsonFromUrl($url);
         return $this->json($json);
     }
